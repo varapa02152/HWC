c698bd0ff0e991fe05436c65233581f52ada691e
Varavaraa <51576721+varapa02152@users.noreply.github.com>
2025-02-24 23:08:18 +0700

    Delete docker/deepseek-docker directory
    Signed-off-by: Varavaraa <51576721+varapa02152@users.noreply.github.com>

--- docker/deepseek-docker/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ollama serve &  # Start Ollama in the background
-sleep 2         # Wait for it to initialize
-ollama pull deepseek-r1  # Pull Llama 3 model
-tail -f /dev/null  # Keep the container running
